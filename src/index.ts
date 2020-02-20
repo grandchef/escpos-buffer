e1f6059922489ed34636512337d13d91a3e87b50
Brad Reed <brad@bradreed.co.uk>
2020-02-20 22:17:25 +0000

    Run Prettier on repo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export * from './connection'
-export { default as Model } from './Model'
-export { default as Image } from './graphics/Image'
-export * from './graphics/filter'
-export * from './Printer'
-export { default as Printer } from './Printer'
+export * from './connection';
+export { default as Model } from './Model';
+export { default as Image } from './graphics/Image';
+export * from './graphics/filter';
+export * from './Printer';
+export { default as Printer } from './Printer';
