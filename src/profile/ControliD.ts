a4841c8f1fb44eebee4ff574865929a1ffc6f2a5
Cristiano Cruz <cristiano@linvix.com.br>
2021-04-22 16:44:06 -0300

    Correção impressão QRCode PrintID

--- src/profile/ControliD.ts
+++ src/profile/ControliD.ts
@@ -20,6 +20,19 @@ export default class ControliD extends Epson {
   }
 
   async qrcode(data: string, size: number) {
-    await this.drawQrcode(data, size);
+    const tipo = '2';
+    const _size = String.fromCharCode(size || 4);
+    const error = '0';
+    const len = data.length + 3;
+    const pL = String.fromCharCode(len & 0xff);
+    const pH = String.fromCharCode((len >> 8) & 0xff);
+    this.connection.write(
+      Buffer.from('\x1D(k\x04\x001A' + tipo + '\x00', 'ascii'),
+    );
+    this.connection.write(Buffer.from('\x1D(k\x03\x001C' + _size, 'ascii'));
+    this.connection.write(Buffer.from('\x1D(k\x03\x001E' + error, 'ascii'));
+    this.connection.write(Buffer.from('\x1D(k' + pL + pH + '1P0', 'ascii'));
+    this.connection.write(Buffer.from(data, 'ascii'));
+    this.connection.write(Buffer.from('\x1D(k\x03\x001Q0', 'ascii'));
   }
 }
