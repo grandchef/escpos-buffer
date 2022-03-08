b1122b588af2c92f7452e7f34fc7de5879bad24b
Theo Gregory <tjmg@outlook.com>
2022-03-08 17:02:47 +0000

    Fix generic bar one

--- __tests__/profile/Generic.spec.ts
+++ __tests__/profile/Generic.spec.ts
@@ -1,36 +1,64 @@
-import Model from '../../src/Model'
-import InMemory from '../../src/connection/InMemory'
-import Printer, { Align, Style } from '../../src/Printer'
-import { load } from '../helper'
+import Model from '../../src/Model';
+import InMemory from '../../src/connection/InMemory';
+import Printer, { Align, Style } from '../../src/Printer';
+import { load } from '../helper';
 
-describe('generic model profile', () => {
-  it('write line text from model CMP-20', () => {
-    const connection = new InMemory()
-    const printer = new Printer(new Model('CMP-20'), connection)
-    printer.writeln('Large Text')
-    expect(connection.buffer()).toStrictEqual(load('cmp-20_text_line', connection.buffer()))
-  })
+describe('generic model profile', async () => {
+  it('write line text from model CMP-20', async () => {
+    const connection = new InMemory();
+    const printer = await Printer.connect(
+      await Model.initialise('CMP-20'),
+      connection,
+    );
+    await printer.writeln('Large Text');
+    expect(connection.buffer()).toStrictEqual(
+      load('cmp-20_text_line', connection.buffer()),
+    );
+  });
 
-  it('write text with double width and height from model CMP-20', () => {
-    const connection = new InMemory()
-    const printer = new Printer(new Model('CMP-20'), connection)
-    printer.writeln('Large Text', Style.DoubleWidth + Style.DoubleHeight, Align.Center)
-    expect(connection.buffer()).toStrictEqual(load('cmp-20_large_text', connection.buffer()))
-  })
+  it('write text with double width and height from model CMP-20', async () => {
+    const connection = new InMemory();
+    const printer = await Printer.connect(
+      await Model.initialise('CMP-20'),
+      connection,
+    );
+    await printer.writeln(
+      'Large Text',
+      Style.DoubleWidth + Style.DoubleHeight,
+      Align.Center,
+    );
+    expect(connection.buffer()).toStrictEqual(
+      load('cmp-20_large_text', connection.buffer()),
+    );
+  });
 
-  it('write text with double width and height from Generic 80mm', () => {
-    const connection = new InMemory()
-    const printer = new Printer(new Model('POS-80'), connection)
-    printer.writeln('Large Text', Style.DoubleWidth + Style.DoubleHeight, Align.Center)
-    expect(connection.buffer()).toStrictEqual(load('generic-80mm_large_text', connection.buffer()))
-  })
+  it('write text with double width and height from Generic 80mm', async () => {
+    const connection = new InMemory();
+    const printer = await Printer.connect(
+      await Model.initialise('POS-80'),
+      connection,
+    );
+    await printer.writeln(
+      'Large Text',
+      Style.DoubleWidth + Style.DoubleHeight,
+      Align.Center,
+    );
+    expect(connection.buffer()).toStrictEqual(
+      load('generic-80mm_large_text', connection.buffer()),
+    );
+  });
 
   it('draw qrcode from model CMP-20', async () => {
-    const connection = new InMemory()
-    const printer = new Printer(new Model('CMP-20'), connection)
-    printer.alignment = Align.Center
-    await printer.qrcode('https://github.com/grandchef/escpos-buffer')
-    printer.alignment = Align.Left
-    expect(connection.buffer()).toStrictEqual(load('cmp-20_qrcode', connection.buffer()))
-  })
-})
+    const connection = new InMemory();
+    const printer = await Printer.connect(
+      await Model.initialise('CMP-20'),
+      connection,
+    );
+    await printer.setAlignment(Align.Center);
+    await printer.qrcode('https://github.com/grandchef/escpos-buffer');
+    await printer.setAlignment(Align.Left);
+    expect(connection.buffer()).toStrictEqual(
+      load('cmp-20_qrcode', connection.buffer()),
+    );
+  });
+});
