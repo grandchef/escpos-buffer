cb6332c4dea02737b77d5285d9df0dc8663d829b
Theo Gregory <tjmg@outlook.com>
2022-03-08 17:01:42 +0000

    Fixes epson tests

--- __tests__/profile/Epson.spec.ts
+++ __tests__/profile/Epson.spec.ts
@@ -4,28 +4,28 @@ import Printer, { Align, Style } from '../../src/Printer';
 import { load } from '../helper';
 
 describe('epson model profile', () => {
-  it('write text from model TM-T20', () => {
+  it('write text from model TM-T20', async () => {
     const connection = new InMemory();
-    const model = new Model('TM-T20');
-    const printer = new Printer(model, connection);
-    printer.writeln('Large Text');
+    const model = await Model.initialise('TM-T20');
+    const printer = await Printer.connect(model, connection);
+    await printer.writeln('Large Text');
     expect(connection.buffer()).toStrictEqual(
       load('tm-t20_text', connection.buffer()),
     );
   });
 
-  it('write text with double width and height from model TM-T20', () => {
+  it('write text with double width and height from model TM-T20', async () => {
     const connection = new InMemory();
-    const model = new Model('TM-T20');
-    const printer = new Printer(model, connection);
-    printer.setColumns = 64;
-    printer.writeln(
+    const model = await Model.initialise('TM-T20');
+    const printer = await Printer.connect(model, connection);
+    await printer.setColumns(64);
+    await printer.writeln(
       'Large Text',
       Style.DoubleWidth + Style.DoubleHeight,
       Align.Center,
     );
-    printer.feed(2);
-    printer.feed(255);
+    await printer.feed(2);
+    await printer.feed(255);
     expect(model.profile.font.name).toBe('Font B');
     expect(connection.buffer()).toStrictEqual(
       load('tm-t20_large_text_font', connection.buffer()),
@@ -34,22 +34,28 @@ describe('epson model profile', () => {
 
   it('draw qrcode from model TM-T20', async () => {
     const connection = new InMemory();
-    const printer = new Printer(new Model('TM-T20'), connection);
-    printer.alignment = Align.Center;
+    const printer = await Printer.connect(
+      await Model.initialise('TM-T20'),
+      connection,
+    );
+    await printer.setAlignment(Align.Center);
     await printer.qrcode('https://github.com/grandchef/escpos-buffer');
-    printer.alignment = Align.Left;
+    await printer.setAlignment(Align.Left);
     expect(connection.buffer()).toStrictEqual(
       load('tm-t20_qrcode', connection.buffer()),
     );
   });
 
-  it('sets char size from the style and clears it after', () => {
+  it('sets char size from the style and clears it after', async () => {
     const connection = new InMemory();
-    const printer = new Printer(new Model('TM-T20'), connection);
+    const printer = await Printer.connect(
+      await Model.initialise('TM-T20'),
+      connection,
+    );
     const cb = jest.fn();
     const width = 4;
     const height = 6;
-    printer.withStyle({ width, height }, cb);
+    await printer.withStyle({ width, height }, cb);
     const expectedN = (height - 1) | ((width - 1) << 4);
 
     const buffer = connection.buffer();
@@ -59,12 +65,15 @@ describe('epson model profile', () => {
     expect(clearCharSizeCmd).toEqual([0x1d, 0x21, 0x00]);
   });
 
-  it('defaults width to 1', () => {
+  it('defaults width to 1', async () => {
     const connection = new InMemory();
-    const printer = new Printer(new Model('TM-T20'), connection);
+    const printer = await Printer.connect(
+      await Model.initialise('TM-T20'),
+      connection,
+    );
     const cb = jest.fn();
     const height = 6;
-    printer.withStyle({ height }, cb);
+    await printer.withStyle({ height }, cb);
 
     const expectedWidth = 1;
     const expectedN = (height - 1) | ((expectedWidth - 1) << 4);
@@ -72,12 +81,15 @@ describe('epson model profile', () => {
     expect(buffer[5]).toBe(expectedN);
   });
 
-  it('defaults height to 1', () => {
+  it('defaults height to 1', async () => {
     const connection = new InMemory();
-    const printer = new Printer(new Model('TM-T20'), connection);
+    const printer = await Printer.connect(
+      await Model.initialise('TM-T20'),
+      connection,
+    );
     const cb = jest.fn();
     const width = 6;
-    printer.withStyle({ width }, cb);
+    await printer.withStyle({ width }, cb);
 
     const expectedHeight = 1;
     const expectedN = (expectedHeight - 1) | ((width - 1) << 4);
@@ -85,11 +97,14 @@ describe('epson model profile', () => {
     expect(buffer[5]).toBe(expectedN);
   });
 
-  it('caps max char size at 8', () => {
+  it('caps max char size at 8', async () => {
     const connection = new InMemory();
-    const printer = new Printer(new Model('TM-T20'), connection);
+    const printer = await Printer.connect(
+      await Model.initialise('TM-T20'),
+      connection,
+    );
     const cb = jest.fn();
-    printer.withStyle(
+    await printer.withStyle(
       {
         width: 15,
         height: 10,
@@ -104,11 +119,14 @@ describe('epson model profile', () => {
     expect(buffer[5]).toBe(expectedN);
   });
 
-  it('caps min char size at 1', () => {
+  it('caps min char size at 1', async () => {
     const connection = new InMemory();
-    const printer = new Printer(new Model('TM-T20'), connection);
+    const printer = await Printer.connect(
+      await Model.initialise('TM-T20'),
+      connection,
+    );
     const cb = jest.fn();
-    printer.withStyle(
+    await printer.withStyle(
       {
         width: -1,
         height: -1,
