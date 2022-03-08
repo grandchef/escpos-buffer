e8a9b890df7224b2972a4740d40aa060083f814a
Theo Gregory <tjmg@outlook.com>
2022-03-08 16:58:23 +0000

    Fix diebold

--- __tests__/profile/Diebold.spec.ts
+++ __tests__/profile/Diebold.spec.ts
@@ -1,20 +1,30 @@
-import Model from '../../src/Model'
-import InMemory from '../../src/connection/InMemory'
-import Printer from '../../src/Printer'
-import { load } from '../helper'
+import Model from '../../src/Model';
+import InMemory from '../../src/connection/InMemory';
+import Printer from '../../src/Printer';
+import { load } from '../helper';
 
 describe('diebold model profile', () => {
-  it('emit buzzer from model TSP-143', () => {
-    const connection = new InMemory()
-    const printer = new Printer(new Model('TSP-143'), connection)
-    printer.buzzer()
-    expect(connection.buffer()).toStrictEqual(load('tsp-143_buzzer', connection.buffer()))
-  })
+  it('emit buzzer from model TSP-143', async () => {
+    const connection = new InMemory();
+    const printer = await Printer.connect(
+      await Model.initialise('TSP-143'),
+      connection,
+    );
+    await printer.buzzer();
+    expect(connection.buffer()).toStrictEqual(
+      load('tsp-143_buzzer', connection.buffer()),
+    );
+  });
 
-  it('activate drawer from model TSP-143', () => {
-    const connection = new InMemory()
-    const printer = new Printer(new Model('TSP-143'), connection)
-    printer.drawer()
-    expect(connection.buffer()).toStrictEqual(load('tsp-143_drawer', connection.buffer()))
-  })
-})
+  it('activate drawer from model TSP-143', async () => {
+    const connection = new InMemory();
+    const printer = await Printer.connect(
+      await Model.initialise('TSP-143'),
+      connection,
+    );
+    await printer.drawer();
+    expect(connection.buffer()).toStrictEqual(
+      load('tsp-143_drawer', connection.buffer()),
+    );
+  });
+});
