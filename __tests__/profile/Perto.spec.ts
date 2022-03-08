38fd14cd70785de528b4c45b65bd4d344baec9c1
Theo Gregory <tjmg@outlook.com>
2022-03-08 17:06:51 +0000

    Fixes perto tests

--- __tests__/profile/Perto.spec.ts
+++ __tests__/profile/Perto.spec.ts
@@ -1,21 +1,31 @@
-import Model from '../../src/Model'
-import InMemory from '../../src/connection/InMemory'
-import Printer, { Align } from '../../src/Printer'
-import { load } from '../helper'
+import Model from '../../src/Model';
+import InMemory from '../../src/connection/InMemory';
+import Printer, { Align } from '../../src/Printer';
+import { load } from '../helper';
 
 describe('perto model profile', () => {
-  it('emit buzzer from model PertoPrinter', () => {
-    const connection = new InMemory()
-    const printer = new Printer(new Model('PertoPrinter'), connection)
-    printer.buzzer()
-    expect(connection.buffer()).toStrictEqual(load('perto_printer_buzzer', connection.buffer()))
-  })
+  it('emit buzzer from model PertoPrinter', async () => {
+    const connection = new InMemory();
+    const printer = await Printer.connect(
+      await Model.initialise('PertoPrinter'),
+      connection,
+    );
+    await printer.buzzer();
+    expect(connection.buffer()).toStrictEqual(
+      load('perto_printer_buzzer', connection.buffer()),
+    );
+  });
 
-  it('cut paper from model PertoPrinter', () => {
-    const connection = new InMemory()
-    const printer = new Printer(new Model('PertoPrinter'), connection)
-    printer.writeln('Cut below', 0, Align.Center)
-    printer.cutter()
-    expect(connection.buffer()).toStrictEqual(load('perto_printer_cutter', connection.buffer()))
-  })
-})
+  it('cut paper from model PertoPrinter', async () => {
+    const connection = new InMemory();
+    const printer = await Printer.connect(
+      await Model.initialise('PertoPrinter'),
+      connection,
+    );
+    await printer.writeln('Cut below', 0, Align.Center);
+    await printer.cutter();
+    expect(connection.buffer()).toStrictEqual(
+      load('perto_printer_cutter', connection.buffer()),
+    );
+  });
+});
