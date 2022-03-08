44b4de3a9491c0afc5c85591a4621dcd8f1daaa9
Theo Gregory <tjmg@outlook.com>
2022-03-08 17:07:32 +0000

    Fix sweta tests

--- __tests__/profile/Sweda.spec.ts
+++ __tests__/profile/Sweda.spec.ts
@@ -1,13 +1,18 @@
-import Model from '../../src/Model'
-import InMemory from '../../src/connection/InMemory'
-import Printer from '../../src/Printer'
-import { load } from '../helper'
+import Model from '../../src/Model';
+import InMemory from '../../src/connection/InMemory';
+import Printer from '../../src/Printer';
+import { load } from '../helper';
 
 describe('sweda model profile', () => {
-  it('emit buzzer from model SI-300L', () => {
-    const connection = new InMemory()
-    const printer = new Printer(new Model('SI-300L'), connection)
-    printer.buzzer()
-    expect(connection.buffer()).toStrictEqual(load('si-300l_buzzer', connection.buffer()))
-  })
-})
+  it('emit buzzer from model SI-300L', async () => {
+    const connection = new InMemory();
+    const printer = await Printer.connect(
+      await Model.initialise('SI-300L'),
+      connection,
+    );
+    printer.buzzer();
+    expect(connection.buffer()).toStrictEqual(
+      load('si-300l_buzzer', connection.buffer()),
+    );
+  });
+});
