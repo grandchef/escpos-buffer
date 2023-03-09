a1a752eab091de4dea2389649e2ffb1ef8d4a0dc
Samuel Durante <samueldurante.h@gmail.com>
2023-03-09 22:51:28 +0000

    style: fix style

--- src/connection/WebBluetooth.ts
+++ src/connection/WebBluetooth.ts
@@ -3,14 +3,16 @@ import { Connection } from '.';
 export default class WebBluetooth implements Connection {
   private characteristic: BluetoothRemoteGATTCharacteristic = null;
 
-  constructor(
-    private device: BluetoothDevice,
-  ) {}
+  constructor(private device: BluetoothDevice) {}
 
   async open(): Promise<void> {
     const server = await this.device.gatt.connect();
-    const service = await server.getPrimaryService('000018f0-0000-1000-8000-00805f9b34fb');
-    const characteristic = await service.getCharacteristic('00002af1-0000-1000-8000-00805f9b34fb');
+    const service = await server.getPrimaryService(
+      '000018f0-0000-1000-8000-00805f9b34fb',
+    );
+    const characteristic = await service.getCharacteristic(
+      '00002af1-0000-1000-8000-00805f9b34fb',
+    );
 
     this.characteristic = characteristic;
   }
